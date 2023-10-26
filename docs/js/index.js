1e89e62170deb17b078ba9aeb86d11a9e0858dd1
Tab Ye <135727531+TabNahida@users.noreply.github.com>
2023-10-26 19:51:54 +0800

    img size

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,10 +1,10 @@
 function WgetMarkDown(Path,ElementId) 
 {
-    fetch(Path)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
+  fetch(Path)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
         return response.text();
       })
       .then(data => {
@@ -15,4 +15,18 @@ function WgetMarkDown(Path,ElementId)
       .catch(error => {
         console.error('Fetch error:', error);
       });
+}
+
+function ImgMatchStyle(src,style)
+{
+  // 查找所有的 <img> 标签
+  var imgTags = document.querySelectorAll('img');
+
+  // 用特定值的 src 来添加样式
+  imgTags.forEach(function(img) {
+    if (img.getAttribute('src') === src) {
+        img.style = style; // 以红色边框为例
+        // 添加其他样式或属性，根据需要
+    }
+  });
 }
